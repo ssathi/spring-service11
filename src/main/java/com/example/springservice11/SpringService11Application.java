a1090fbb0e5d42d58b2c16107a71c639460192b7
sathi2 <sathi@wired2perform.com>
2019-02-25 08:46:34 +0530

    New service 11. jx split shoild run - ee8b7644c6d6608bbe0f5d32a20d038c67383f59

---
+++ src/main/java/com/example/springservice11/SpringService11Application.java
@@ -0,0 +1,13 @@
+package com.example.springservice11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringService11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringService11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springservice11/SpringService11ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springservice11;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringService11ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
